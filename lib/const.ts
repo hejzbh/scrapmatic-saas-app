85697eb51cb4090264c48be154b4a07080ccd929
hejzbh <hazimba123@gmail.com>
2025-01-20 21:06:00 +0100

    edit

--- lib/const.ts
+++ lib/const.ts
@@ -4,7 +4,7 @@ export const PAGINATION_PER_PAGE = 10;
 
 export const CREDITS_COSTS = {
   tasks: {
-    [FlowNodeTaskType.LAUNCH_BROWSER]: 40000,
+    [FlowNodeTaskType.LAUNCH_BROWSER]: 4,
     [FlowNodeTaskType.PAGE_TO_HTML]: 4,
     [FlowNodeTaskType.EXTRACT_TEXT_FROM_ELEMENT]: 2,
   },
